741d6a5704e2fd2b408a81af9fd3aca8d03522ea
Abdul <abdulbasirqazi7896@gmail.com>
2023-05-21 02:11:04 +0500

    Now the teacher profile is working well. Just one problem fixed it later. Now start working on dairy

--- controller/school_admin_controls.py
+++ controller/school_admin_controls.py
@@ -35,6 +35,7 @@ def student_admission():
 
 
 @app.route('/school_admin/update_admission_data' , methods=["GET", "POST"])
+@login_required('school_admin')
 def update_admission_data():
     if request.method == "GET":
         # flash("Welcome to the website!", "success")
@@ -47,4 +48,38 @@ def update_admission_data():
         return render_template("school_admin_URLs/student_admission.html" , data = result)
         
     return render_template("school_admin_URLs/update_admission_data.html")
+
+
+
+
+@app.route('/school_admin/Teacher_Joining' , methods=["GET", "POST"])
+@login_required('school_admin')
+def teacher_joining_information():
+    data = [('', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '')]
+    if request.method == "GET":
+        # flash("Welcome to the website!", "success")
+        return render_template("school_admin_URLs/teacher_joining.html" ,  data = data )
     
+    elif request.method == 'POST':
+        dataa = request.form.to_dict()
+        obj.teacher_joining_information(dataa)
+    return render_template("school_admin_URLs/teacher_joining.html" ,  data = data)
+
+
+
+
+@app.route('/school_admin/update_teacher_data' , methods=["GET", "POST"])
+@login_required('school_admin')
+def update_teacher_data():
+    if request.method == "GET":
+        # flash("Welcome to the website!", "success")
+        return render_template("school_admin_URLs/update_teacher_data.html")
+    
+    elif request.method == 'POST':
+        data = request.form.to_dict()
+        result = obj.search_teacher_data(data)
+        print("This is resut = " , result)
+        return render_template("school_admin_URLs/teacher_joining.html" , data = result)
+        
+    return render_template("school_admin_URLs/update_teacher_data.html")
+
