f5dbf6866480c84888ef023250b2a2929fa43f90
Abdul <abdulbasirqazi7896@gmail.com>
2023-06-18 14:30:29 +0500

    Now just teacher error are left

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask , render_template , request
+from sqlalchemy.exc import OperationalError
 from model.user_model import user_model
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
@@ -10,6 +11,11 @@ def hello_world():
     return render_template('index.html' , posts = cheek)
 
 from controller import *
+
+# @app.errorhandler(OperationalError)
+# def handle_operational_error(error):
+#     app.logger.error(f"OperationalError: {str(error)}")
+#     return render_template('error.html', message="An error occurred while processing your request."), 500
 
 
 if __name__ == '__main__':
